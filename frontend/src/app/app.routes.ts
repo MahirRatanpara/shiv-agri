b6ee0b2cf14fd236ee0f1bd32b3734f0b5a11a31
mahirratanpara <mahirratanpara1313@gmail.com>
2025-10-26 21:52:49 +0530

    homepage and contact page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { EventsComponent } from './pages/events/events';
+import { EventDetailsComponent } from './pages/event-details/event-details';
+import { CausesComponent } from './pages/causes/causes';
+import { CausesDetailsComponent } from './pages/causes-details/causes-details';
+import { BlogComponent } from './pages/blog/blog';
+import { BlogDetailsComponent } from './pages/blog-details/blog-details';
+import { ProjectsComponent } from './pages/projects/projects';
+import { ProjectDetailsComponent } from './pages/project-details/project-details';
+import { ShopComponent } from './pages/shop/shop';
+import { ShopDetailsComponent } from './pages/shop-details/shop-details';
+import { TeamComponent } from './pages/team/team';
+import { TeamDetailsComponent } from './pages/team-details/team-details';
+import { GalleryComponent } from './pages/gallery/gallery';
+import { TestimonialsComponent } from './pages/testimonials/testimonials';
+import { DonationComponent } from './pages/donation/donation';
+import { MyAccountComponent } from './pages/my-account/my-account';
+import { ContactComponent } from './pages/contact/contact';
+import { NotFoundComponent } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', redirectTo: '/contact', pathMatch: 'full' },
+  { path: 'events', component: EventsComponent },
+  { path: 'event-details/:id', component: EventDetailsComponent },
+  { path: 'causes', component: CausesComponent },
+  { path: 'causes-details/:id', component: CausesDetailsComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'blog-details/:id', component: BlogDetailsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'project-details/:id', component: ProjectDetailsComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'shop-details/:id', component: ShopDetailsComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'team-details/:id', component: TeamDetailsComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'testimonials', component: TestimonialsComponent },
+  { path: 'donation', component: DonationComponent },
+  { path: 'my-account', component: MyAccountComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
+];
---
+++ frontend/src/app/pages/causes/causes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-causes',
+  imports: [],
+  templateUrl: './causes.html',
+  styleUrl: './causes.css',
+})
+export class CausesComponent {
+
+}
